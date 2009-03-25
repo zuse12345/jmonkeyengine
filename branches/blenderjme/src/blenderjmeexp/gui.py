bb693985f56e0aad3311f79eefa5475bc5d9a4ef
blaine.dev <blaine.dev@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2009-03-25 19:36:16 +0000

    Properly copyrighted to me and the jME team via BSD license

--- branches/blenderjme/src/blenderjmeexp/gui.py
+++ branches/blenderjme/src/blenderjmeexp/gui.py
@@ -3,6 +3,33 @@ __date__ = '$Date$'
 __author__ = 'Blaine Simpson, blaine (dot) simpson (at) admc (dot) com'
 __url__ = 'http://www.jmonkeyengine.com'
 
+# Copyright (c) 2009, Blaine Simpson and the jMonkeyEngine team
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY Blaine Simpson and the jMonkeyEngine team
+# ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL Blaine Simpson or the jMonkeyEngine team
+# BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import Blender
 from Blender import Draw
 from Blender import BGL
@@ -179,6 +206,10 @@ def drawer():
     guiBox.drawBg()
     Draw.PushButton("Cancel", BTNID_CANCEL, \
             guiBox.x + 10, guiBox.y + 10, 50, 20, "Abort export")
+    Draw.Label(" (c) 2009 Blaine Simpson", \
+            guiBox.x + 170, guiBox.y + 23,145,10)
+    Draw.Label("+ the jMonkeyEngine team", \
+            guiBox.x + 160, guiBox.y + 9,157,10)
     if not allCount: updateExportableCounts()
     if allCount < 1:  # TEMPORARY.  Enable following once impl. saveAll.
         Draw.Label("Your scenes contain no",

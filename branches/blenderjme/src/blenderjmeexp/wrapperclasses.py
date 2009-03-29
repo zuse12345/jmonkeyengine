d8b71061518fe32117593cbbf0367a182c8c6a7d
blaine.dev <blaine.dev@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2009-03-29 20:00:22 +0000

    Rename wrapped classes consistently to correspond to their final JME classes. Generalized JmeNode to work for both grouping-only and mesh-containing nodes. Both JmeNode and JmeMesh now have a modifiable (overridable) name. Unfortunately, added some more significant TODO items... seem to be getting farther and farther behind.

--- branches/blenderjme/src/blenderjmeexp/wrapperclasses.py
+++ branches/blenderjme/src/blenderjmeexp/wrapperclasses.py
@@ -37,52 +37,62 @@ from jme.xml import *
 from Blender.Mathutils import Quaternion
 from jme.esmath import *
 
-class JmeObject(object):
-    __slots__ = ('wrappedObj', 'children', '__vpf')
+# GENERAL DEVELOPMENT NOTES:
+#
+# When getting the mesh to work with from Blender, make sure to use
+# ...getMesh(False, True), or the equivalent ...getMesh(mesh=True).
+# This will get a Blender "wrapped" Mesh instance instead of a non-wrapped
+# NMesh instance (contrary to the wrapping we are doing here, Blender's
+# "wrapping" actually puts you closer to the physical data).
+# The parent/child relationships between these wrapper classes reflects
+# scene node relationships, not Blender's explicit parenting relationships.
+# For example, Blender Objects aggregate Meshes and Mesh references, but do
+# not consider them children; but in JME, nodes can have nodes and Meshes
+# (or any other Spatials) as children.
+
+class JmeNode(object):
+    __slots__ = ('wrappedObj', 'children', 'name')
     __QUAT_IDENTITY = ESQuaternion([0,0,0], 1)  # Do not modify value!
 
-    def __init__(self, bObj):
-        """Assumes input Blender Object already validated.
+    def __init__(self, bObjOrName):
+        """Assumes input Blender Object already validated, like by using the
+        supported() static method below.
         I.e., is of supported type, and facing method is supported."""
         object.__init__(self)
-        self.wrappedObj = bObj
         self.children = None
-        print "Instantiated JmeObject '" + self.getName() + "'"
-        self.__vpf = JmeObject.__vertsPerFace(bObj.getData(False, True).faces)
-        # TODO:  Generate vertex color array by looking up each vertex's
-        # color in the face's face.col[vertIndex].
-        # What to do if some vertexes have color (by associated with a face
-        # vertex color) but other vertexes in the same mesh do not?...
-        # I'm thinking to save them as white and at least print a warning
-        # to stdout.  Looks like existing JME XML parser requires color
-        # coordinates for all vertexes or none.
+        self.wrappedObj = None
+
+        if isinstance(bObjOrName, basestring):
+            self.name = bObjOrName
+        else:
+            self.wrappedObj = bObjOrName
+            self.name = self.wrappedObj.name
+            bMesh = self.wrappedObj.getData(False, True)
+            if bMesh != None:
+                self.addChild(JmeMesh(bMesh, self.wrappedObj.color))
+        print "Instantiated JmeNode '" + self.getName() + "'"
 
     def addChild(self, child):
         if self.children == None: self.children = []
         self.children.append(child)
 
-    def getName(self):
-        return self.wrappedObj.name
+    def getName(self): return self.name
 
     def getXmlEl(self):
         tag = XmlTag('com.jme.scene.Node', {'name':self.getName()})
-        # TODO:  This is where all of the attributes and children should be
-        # added to the XML.
-        mesh = self.wrappedObj.getData(False, True)
-        if mesh != None:
-            childrenTag = XmlTag("children", {"size":1})
-            childrenTag.addChild(JmeObject.__genMeshEl( \
-                    mesh, self.__vpf, self.wrappedObj.color))
-            tag.addChild(childrenTag)
 
         if self.children != None:
-            for child in self.children: tag.addChild(child.getXmlEl())
+            childrenTag = XmlTag('children', {'size':len(self.children)})
+            tag.addChild(childrenTag)
+            for child in self.children: childrenTag.addChild(child.getXmlEl())
+        if self.wrappedObj == None: return tag
+
         # Use either loc + rot + size OR matrixLocal
         # Set local variables just to reduce typing in this block.
         loc = self.wrappedObj.loc
         # N.b. Blender.Mathutils.Quaternines ARE NOT COMPATIBLE WITH jME!
         rQuat = ESQuaternion(self.wrappedObj.rot)
-        if rQuat == JmeObject.__QUAT_IDENTITY: rQuat = None
+        if rQuat == JmeNode.__QUAT_IDENTITY: rQuat = None
         size = self.wrappedObj.size
         # Need to add the attrs sequentially in order to preserve sequence
         # of the attrs in the output.
@@ -110,23 +120,13 @@ class JmeObject(object):
     def getType(self):
         return self.wrappedObj.type
 
-    def __vertsPerFace(faces):
-        """Returns 0 if type not supported; non-0 if supported.
-           2 if will require face niggling; 3 if faceless"""
-        if faces == None: return None
-        facings = set()
-        for f in faces:
-            if f.verts == None: raise Exception("Face with no vertexes?")
-            facings.add(len(f.verts))
-        return facings
-
     def supported(bObj):
         """Returns 0 if type not supported; non-0 if supported.
            2 if will require face niggling; 3 if faceless"""
         if bObj.type not in ['Mesh']: return 0
         if not bObj.getData(False, True):
             raise Exception("Mesh Object has no data member?")
-        vpf = JmeObject.__vertsPerFace(bObj.getData(False, True).faces)
+        vpf = JmeMesh.vertsPerFace(bObj.getData(False, True).faces)
         if vpf == None:
             print "FYI:  Accepting object '" + bObj.name + "' with no faces"
             return 3
@@ -147,44 +147,69 @@ class JmeObject(object):
         return '[' + self.getName() + ']'
 
     def __repr__(self):
-        return "<JmeObject> " + self.__str__()
+        return "<JmeNode> " + self.__str__()
 
-    def __genMeshEl(meshObj, vpf, color):
-        if meshObj.verts == None:
-            raise Exception("Mesh '" + meshObj.name + "' has no vertexes")
-        unify = 3 in vpf and 4 in vpf
-        if 4 in vpf and not unify:
+    supported = staticmethod(supported)
+
+
+class JmeMesh(object):
+    __slots__ = ('wrappedMesh', '__vpf', 'defaultColor', 'name')
+    # defaultColor corresponds to jME's Meshs' defaultColor.
+    # In Blender this is a per-Object, not per-Mesh setting.
+    # This is why it is a parameter of the constructor below.
+
+    def __init__(self, bMesh, color=None):
+        """Assumes input Blender Mesh already validated, like by using the
+        vertsPerFace static method below.
+        I.e., is of supported type, and facing method is supported.
+        The color setting should come from the parent Blender Object's
+        .color setting."""
+        object.__init__(self)
+        self.wrappedMesh = bMesh
+        self.name = bMesh.name
+        self.defaultColor = color
+        print "Instantiated JmeMesh '" + self.getName() + "'"
+        self.__vpf = JmeMesh.vertsPerFace(self.wrappedMesh.faces)
+
+    def getName(self): return self.name
+
+    def getXmlEl(self):
+        if self.wrappedMesh.verts == None:
+            raise Exception("Mesh '" + self.getName() + "' has no vertexes")
+        unify = 3 in self.__vpf and 4 in self.__vpf
+        if 4 in self.__vpf and not unify:
             meshType = 'Quad'
         else:
             meshType = 'Tri'
         # TODO:  When iterate through verts to get vert vectors + normals,
         #        do check for null normal and throw if so:
         #   raise Exception("Mesh '" \
-        #       + meshObj.name + "' has a vector with no normal")
+        #       + self.wrappedMesh.name + "' has a vector with no normal")
         #   This is a Blender convention, not a 3D or JME convention
         #   (requirement for normals).
-        tag = XmlTag('com.jme.scene.' + meshType + 'Mesh', {'name':meshObj.name})
-        if color != None and \
-            (color[0] != 1 or color[1] != 1 or color[2] != 1 or color[3] != 1):
+        tag = XmlTag('com.jme.scene.' + meshType + 'Mesh',
+                {'name':self.getName()})
+        if self.defaultColor != None and \
+            (self.defaultColor[0] != 1 or self.defaultColor[1] != 1 or self.defaultColor[2] != 1 or self.defaultColor[3] != 1):
             colorTag = XmlTag("defaultColor", \
                     {"class":"com.jme.renderer.ColorRGBA"})
             tag.addChild(colorTag)
-            colorTag.addAttr("r", color[0])
-            colorTag.addAttr("g", color[1])
-            colorTag.addAttr("b", color[2])
-            colorTag.addAttr("a", color[3])
+            colorTag.addAttr("r", self.defaultColor[0])
+            colorTag.addAttr("g", self.defaultColor[1])
+            colorTag.addAttr("b", self.defaultColor[2])
+            colorTag.addAttr("a", self.defaultColor[3])
 
         vcMap = None   # maps vertex INDEX to MCol
         colArray = None
-        if meshObj.vertexColors:
+        if self.wrappedMesh.vertexColors:
             multiColorMapped = False
             vcMap = {}
             # TODO:  Test for objects with no faces, like curves, points.
-            for face in meshObj.faces:
+            for face in self.wrappedMesh.faces:
                 if face.verts == None: continue
                 # The face.col reference in the next line WILL THROW
                 # if the mesh does not support vert colors
-                # (i.e. meshObj.vertexColors).
+                # (i.e. self.wrappedMesh.vertexColors).
                 if len(face.verts) != len(face.col):
                     raise Exception( \
                     "Counts of Face vertexes and vertex-colors do not match: " \
@@ -200,7 +225,7 @@ class JmeObject(object):
         coArray = []
         noArray = []
         nonFacedVertexes = 0
-        for v in meshObj.verts:
+        for v in self.wrappedMesh.verts:
             coArray.append(v.co.x)
             coArray.append(v.co.y)
             coArray.append(v.co.z)
@@ -238,9 +263,9 @@ class JmeObject(object):
             vertColTag = XmlTag("colorBuf", {"data":rgbaArray}, 3)
             vertColTag.addAttr("size", len(rgbaArray))
             tag.addChild(vertColTag)
-        if 3 not in vpf and 4 not in vpf: return tag
+        if 3 not in self.__vpf and 4 not in self.__vpf: return tag
         faceVertIndexes = []
-        for face in meshObj.faces:
+        for face in self.wrappedMesh.faces:
             if face.verts == None or len(face.verts) < 1: continue
             if unify and len(face.verts) == 4:
                 faceVertIndexes.append(face.verts[0].index)
@@ -256,41 +281,14 @@ class JmeObject(object):
         tag.addChild(indTag)
         return tag
 
-    supported = staticmethod(supported)
-    __genMeshEl = staticmethod(__genMeshEl)
-    __vertsPerFace = staticmethod(__vertsPerFace)
+    def vertsPerFace(faces):
+        """Returns 0 if type not supported; non-0 if supported.
+           2 if will require face niggling; 3 if faceless"""
+        if faces == None: return None
+        facings = set()
+        for f in faces:
+            if f.verts == None: raise Exception("Face with no vertexes?")
+            facings.add(len(f.verts))
+        return facings
 
-class JmeNode(object):
-    __slots__ = ('name', 'children')
-
-    def __init__(self, name):
-        object.__init__(self)
-        self.name = name
-        self.children = None
-        print "Instantiated JmeNode '" + self.getName() + "'"
-
-    def addChild(self, child):
-        if not self.children: self.children = []
-        self.children.append(child)
-
-    def getName(self):
-        return self.name
-
-    def getType(self):
-        return self.wrappedObj.type
-
-    def __str__(self):
-        return '(' + self.getName() + ')'
-
-    def __repr__(self):
-        return "<JmeNode> " + self.__str__()
-
-    def getXmlEl(self):
-        tag = XmlTag('com.jme.scene.Node', {'name':self.getName()})
-        # TODO:  This is where all of the attributes and children should be
-        # added to the XML.
-        if self.children != None:
-            childrenTag = XmlTag('children', {'size':len(self.children)})
-            tag.addChild(childrenTag)
-            for child in self.children: childrenTag.addChild(child.getXmlEl())
-        return tag
+    vertsPerFace = staticmethod(vertsPerFace)

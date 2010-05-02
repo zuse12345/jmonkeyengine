96af8d6bf5ea2eb9c3d41a6a2c05903fb43906b3
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-05-02 14:28:00 +0000

    jme3-gde: - fix j3m opening in editor

--- branches/jme3-gde/jme3-core/src/com/jme3/gde/core/filetypes/JMEMaterialDataObject.java
+++ branches/jme3-gde/jme3-core/src/com/jme3/gde/core/filetypes/JMEMaterialDataObject.java
@@ -35,12 +35,16 @@ import java.io.IOException;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiFileLoader;
+import org.openide.nodes.CookieSet;
+import org.openide.nodes.Node;
+import org.openide.text.DataEditorSupport;
 
 public class JMEMaterialDataObject extends AssetDataObject {
 
     public JMEMaterialDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
         super(pf, loader);
-
+        CookieSet cookies = getCookieSet();
+        cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
     }
 
 }

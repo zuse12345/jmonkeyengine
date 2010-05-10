872b6ca9facf0f227a2a8fce61a28d58b80de4e1
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-05-10 12:56:27 +0000

    jme3: - move TestQ3 to bullet character

--- branches/jme3/src/test/jme3test/scene/TestQ3.java
+++ branches/jme3/src/test/jme3test/scene/TestQ3.java
@@ -1,26 +1,35 @@
 package jme3test.scene;
 
-import com.jme3.app.SimpleApplication;
+import com.jme3.app.SimpleBulletApplication;
 import com.jme3.asset.plugins.ZipLocator;
-import com.jme3.collision.MotionAllowedListener;
+import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.nodes.PhysicsCharacterNode;
+import com.jme3.bullet.nodes.PhysicsNode;
+import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.FirstPersonCamera;
+import com.jme3.input.KeyInput;
+import com.jme3.input.binding.BindingListener;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.plugins.ogre.MeshLoader;
 import com.jme3.scene.plugins.ogre.OgreMaterialList;
 import com.jme3.scene.plugins.ogre.OgreMeshKey;
 import com.jme3.scene.shape.Sphere;
-import jme3test.collision.SphereMotionAllowedListener;
 
-public class TestQ3 extends SimpleApplication {
+public class TestQ3 extends SimpleBulletApplication implements BindingListener{
 
     private Sphere sphereMesh = new Sphere(32, 32, 10f, false, true);
     private Geometry sphere = new Geometry("Sky", sphereMesh);
     private Spatial gameLevel;
+    private PhysicsCharacterNode player;
+    private Vector3f walkDirection=new Vector3f();
 
     public static void main(String[] args){
         TestQ3 app = new TestQ3();
@@ -28,9 +37,9 @@ public class TestQ3 extends SimpleApplication {
     }
 
     public void simpleInitApp() {
-        MeshLoader.AUTO_INTERLEAVE = false;
-//        this.flyCam.setMoveSpeed(500);
         inputManager.removeBindingListener(flyCam);
+        setupKeys();
+        MeshLoader.AUTO_INTERLEAVE = false;
         FirstPersonCamera fps = new FirstPersonCamera(cam, new Vector3f(0, -10, 0));
         fps.registerWithDispatcher(inputManager);
         fps.setMoveSpeed(100);
@@ -63,22 +72,70 @@ public class TestQ3 extends SimpleApplication {
         OgreMeshKey key = new OgreMeshKey("main.meshxml", matList);
         gameLevel = (Spatial) assetManager.loadAsset(key);
         gameLevel.updateGeometricState();
-//        gameLevel.setLocalScale(0.2f);
-        rootNode.attachChild(gameLevel);
+
+        CompoundCollisionShape levelShape=CollisionShapeFactory.createMeshCompoundShape((Node)gameLevel);
+
+        //TODO: values are too high, level is too large,
+        //      scaling not supported yet with CollisionShapeFactory.createMeshCompoundShape
+        PhysicsNode levelNode=new PhysicsNode(gameLevel, levelShape,0);
+        player=new PhysicsCharacterNode(new SphereCollisionShape(60), 0.1f);
+        player.setGravity(100);
+
+        player.setLocalTranslation(new Vector3f(600,100,-600));
+        player.updateGeometricState();
+
+        rootNode.attachChild(levelNode);
+        rootNode.attachChild(player);
         rootNode.updateGeometricState();
 
-//        cam.setLocation(new Vector3f(-205, 47, -1175));
-        //cam.setLocation(new Vector3f(0, 750, 0));
-        //cam.setRotation(new Quaternion(0.0073885284f, -0.67373353f, 0.006737455f, 0.7389067f));
-        cam.setLocation(new Vector3f(340, 264, 453));
-        cam.setRotation(new Quaternion(-2.97E-4f, 0.999f, -0.0349f, -0.0085f));
-        MotionAllowedListener motAllow = new SphereMotionAllowedListener(rootNode, new Vector3f(100, 200, 100));
-        fps.setMotionAllowedListener(motAllow);
-        //flyCam.setMotionAllowedListener();
+        getPhysicsSpace().add(levelNode);
+        getPhysicsSpace().add(player);
+
+//        MotionAllowedListener motAllow = new SphereMotionAllowedListener(rootNode, new Vector3f(100, 200, 100));
+//        fps.setMotionAllowedListener(motAllow);
     }
 
     @Override
     public void simpleUpdate(float tpf){
-        sphere.setLocalTranslation(cam.getLocation());
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getLocalTranslation());
+    }
+
+    private void setupKeys() {
+        inputManager.registerKeyBinding("Lefts", KeyInput.KEY_H);
+        inputManager.registerKeyBinding("Rights", KeyInput.KEY_K);
+        inputManager.registerKeyBinding("Ups", KeyInput.KEY_U);
+        inputManager.registerKeyBinding("Downs", KeyInput.KEY_J);
+        inputManager.registerKeyBinding("Space", KeyInput.KEY_SPACE);
+        //used with method onBinding in BindingListener interface
+        //in order to add function to keys
+        inputManager.addBindingListener(this);
+    }
+
+    public void onBinding(String binding, float value) {
+        if(binding.equals("Lefts")){
+            Quaternion quat=new Quaternion(0, 1, 0, FastMath.QUARTER_PI);
+            walkDirection.addLocal(quat.mult(cam.getDirection().mult(2)));
+        }
+        else if(binding.equals("Rights")){
+            Quaternion quat=new Quaternion(0, 1, 0, -FastMath.QUARTER_PI);
+            walkDirection.addLocal(quat.mult(cam.getDirection().mult(2)));
+        }
+        else if(binding.equals("Ups")){
+            walkDirection.addLocal(cam.getDirection().mult(2));
+        }
+        else if(binding.equals("Downs")){
+            walkDirection.addLocal(cam.getDirection().mult(-2));
+        }
+        else if(binding.equals("Space")){
+            player.jump();
+        }
+    }
+
+    public void onPreUpdate(float tpf) {
+        walkDirection.set(0,0,0);
+    }
+
+    public void onPostUpdate(float tpf) {
     }
 }

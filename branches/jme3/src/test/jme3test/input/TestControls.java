ed2e1d40398469a8dbaa235e2f43549663af6962
rootli <rootli@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-06-30 14:58:44 +0000

    updated test: input control system is now part of inputManager

--- branches/jme3/src/test/jme3test/input/TestControls.java
+++ branches/jme3/src/test/jme3test/input/TestControls.java
@@ -5,16 +5,10 @@ import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
-import com.jme3.system.AppSettings;
-import com.jme3.input.controls.Controls;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseAxisTrigger;
-import com.jme3.input.lwjgl.LwjglKeyInput;
-import com.jme3.input.lwjgl.LwjglMouseInput;
 
 public class TestControls extends SimpleApplication {
-
-    private Controls controls;
     
     private ActionListener actionListener = new ActionListener(){
         public void onAction(String name, boolean pressed, float tpf){
@@ -29,30 +23,19 @@ public class TestControls extends SimpleApplication {
 
     public static void main(String[] args){
         TestControls app = new TestControls();
-        AppSettings settings = new AppSettings(true);
-        settings.setUseInput(false);
-//        settings.setFrameRate(15);
-        app.setSettings(settings);
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
-        controls = new Controls(new LwjglKeyInput(), new LwjglMouseInput(), null);
-
         // Test multiple inputs per mapping
-        controls.addMapping("Space", new KeyTrigger(KeyInput.KEY_SPACE),
-                                     new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
+        inputManager.addMapping("My Action",
+                new KeyTrigger(KeyInput.KEY_SPACE),
+                new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
 
         // Test multiple listeners per mapping
-        controls.addListener(actionListener, "Space");
-        controls.addListener(analogListener, "Space");
-    }
-
-    @Override
-    public void simpleUpdate(float tpf){
-        // You won't have to do this when the system becomes core.
-        controls.update(tpf);
+        inputManager.addListener(actionListener, "My Action");
+        inputManager.addListener(analogListener, "My Action");
     }
 
 }

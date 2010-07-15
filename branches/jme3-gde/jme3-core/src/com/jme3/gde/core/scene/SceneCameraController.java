1d3baf6d85e28b5be66dae8da389085b1c47768d
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-07-15 14:14:05 +0000

    jme3-gde: - commit SceneCameraController

--- branches/jme3-gde/jme3-core/src/com/jme3/gde/core/scene/SceneCameraController.java
+++ branches/jme3-gde/jme3-core/src/com/jme3/gde/core/scene/SceneCameraController.java
@@ -32,8 +32,11 @@
 package com.jme3.gde.core.scene;
 
 import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
-import com.jme3.input.binding.BindingListener;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
@@ -43,12 +46,12 @@ import com.jme3.renderer.Camera;
  *
  * @author normenhansen
  */
-public class SceneCameraController implements BindingListener{
+public class SceneCameraController implements ActionListener, AnalogListener {
 
     private boolean leftMouse, rightMouse, middleMouse;
-    private float deltaX, deltaY, deltaZ, deltaWheel;
-    private int mouseX = 0;
-    private int mouseY = 0;
+    private float deltaX = 0.01f, deltaY = 0.01f, deltaZ = 0.01f, deltaWheel = 1;
+    private float mouseX = 0;
+    private float mouseY = 0;
     private Quaternion rot = new Quaternion();
     private Vector3f vector = new Vector3f();
     private Vector3f focus = new Vector3f();
@@ -59,28 +62,31 @@ public class SceneCameraController implements BindingListener{
         this.cam = cam;
         this.inputManager = inputManager;
 
-        inputManager.registerMouseAxisBinding("MOUSE_X+", 0, false);
-        inputManager.registerMouseAxisBinding("MOUSE_X-", 0, true);
-        inputManager.registerMouseAxisBinding("MOUSE_Y+", 1, false);
-        inputManager.registerMouseAxisBinding("MOUSE_Y-", 1, true);
-        inputManager.registerMouseAxisBinding("MOUSE_W+", 2, false);
-        inputManager.registerMouseAxisBinding("MOUSE_W-", 2, true);
-
-        inputManager.registerMouseButtonBinding("MOUSE_LEFT", 0);
-        inputManager.registerMouseButtonBinding("MOUSE_RIGHT", 1);
-        inputManager.registerMouseButtonBinding("MOUSE_MIDDLE", 2);
-
-        inputManager.registerKeyBinding("Up", KeyInput.KEY_UP);
-        inputManager.registerKeyBinding("Down", KeyInput.KEY_DOWN);
-
+        inputManager.addMapping("MouseAxisX", new MouseAxisTrigger(MouseInput.AXIS_X, false));
+        inputManager.addMapping("MouseAxisY", new MouseAxisTrigger(MouseInput.AXIS_Y, false));
+        inputManager.addMapping("MouseAxisX-", new MouseAxisTrigger(MouseInput.AXIS_X, true));
+        inputManager.addMapping("MouseAxisY-", new MouseAxisTrigger(MouseInput.AXIS_Y, true));
+        inputManager.addMapping("MouseWheel", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
+        inputManager.addMapping("MouseWheel-", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, true));
+        inputManager.addMapping("MouseButtonLeft", new MouseButtonTrigger(0));
+        inputManager.addMapping("MouseButtonMiddle", new MouseButtonTrigger(1));
+        inputManager.addMapping("MouseButtonRight", new MouseButtonTrigger(2));
     }
 
-    public void enable(){
-        inputManager.addBindingListener(this);
+    public void enable() {
+        inputManager.addListener(this, "MouseAxisX");
+        inputManager.addListener(this, "MouseAxisY");
+        inputManager.addListener(this, "MouseAxisX-");
+        inputManager.addListener(this, "MouseAxisY-");
+        inputManager.addListener(this, "MouseWheel");
+        inputManager.addListener(this, "MouseWheel-");
+        inputManager.addListener(this, "MouseButtonLeft");
+        inputManager.addListener(this, "MouseButtonMiddle");
+        inputManager.addListener(this, "MouseButtonRight");
     }
 
-    public void disable(){
-        inputManager.removeBindingListener(this);
+    public void disable() {
+        inputManager.removeListener(this);
     }
 
     private void rotateCamera(Vector3f axis, float amount) {
@@ -119,57 +125,69 @@ public class SceneCameraController implements BindingListener{
         cam.setLocation(loc);
     }
 
-    public void onBinding(String binding, float value) {
-        if (binding.equals("UPDATE")) {
-        } else if (binding.equals("Up")) {
-            deltaZ = 10;
-        } else if (binding.equals("Down")) {
-            deltaZ = -10;
-        } else if (binding.equals("MOUSE_LEFT")) {
-            leftMouse = value > 0f;
-        } else if (binding.equals("MOUSE_RIGHT")) {
-            rightMouse = value > 0f;
-        } else if (binding.equals("MOUSE_MIDDLE")) {
-            middleMouse = value > 0f;
-        } else if (binding.equals("MOUSE_X+")) {
-            deltaX = value;
-        } else if (binding.equals("MOUSE_X-")) {
-            deltaX = -value;
-        } else if (binding.equals("MOUSE_Y+")) {
-            deltaY = value;
-        } else if (binding.equals("MOUSE_Y-")) {
-            deltaY = -value;
-        } else if (binding.equals("MOUSE_W+")) {
-            deltaWheel = value;
-        } else if (binding.equals("MOUSE_W-")) {
-            deltaWheel = -value;
+    public void onAction(String string, boolean bln, float f) {
+        if ("MouseButtonLeft".equals(string)) {
+            if (bln) {
+                leftMouse = true;
+            } else {
+                leftMouse = false;
+            }
+        }
+        if ("MouseButtonRight".equals(string)) {
+            if (bln) {
+                rightMouse = true;
+            } else {
+                rightMouse = false;
+            }
         }
     }
 
-    public void onPreUpdate(float f) {
-        if (leftMouse) {
-            rotateCamera(Vector3f.UNIT_Y, -deltaX * 5);
-            rotateCamera(cam.getLeft(), -deltaY * 5);
+    public void onAnalog(String string, float f, float f1) {
+        if ("MouseAxisX".equals(string)) {
+            //deltaX=f1*100.0f;
+            mouseX=f;
+            if (leftMouse) {
+                rotateCamera(Vector3f.UNIT_Y, -f1 * 1);
+            }
+            if (rightMouse) {
+                panCamera(deltaX * 10, -deltaY * 10);
+            }
         }
-        if (deltaWheel != 0) {
-            zoomCamera(deltaWheel * 10);
+        else if ("MouseAxisY".equals(string)) {
+            mouseY=f;
+            //deltaY=f1*100.0f;
+            if (leftMouse) {
+                rotateCamera(cam.getLeft(), -f1 * 1);
+            }
+            if (rightMouse) {
+                panCamera(deltaX * 10, -deltaY * 10);
+            }
         }
-        if (rightMouse) {
-            panCamera(deltaX * 10, -deltaY * 10);
+        else if ("MouseAxisX-".equals(string)) {
+            //deltaX=f1*100.0f;
+            mouseX=f;
+            if (leftMouse) {
+                rotateCamera(Vector3f.UNIT_Y, f1 * 1);
+            }
+            if (rightMouse) {
+                panCamera(deltaX * 10, -deltaY * 10);
+            }
         }
-
-        moveCamera(deltaZ);
-
-        leftMouse = false;
-        rightMouse = false;
-        middleMouse = false;
-        deltaX = 0;
-        deltaY = 0;
-        deltaZ = 0;
-        deltaWheel = 0;
+        else if ("MouseAxisY-".equals(string)) {
+            mouseY=f;
+            //deltaY=f1*100.0f;
+            if (leftMouse) {
+                rotateCamera(cam.getLeft(), f1 * 1);
+            }
+            if (rightMouse) {
+                panCamera(deltaX * 10, -deltaY * 10);
+            }
+        }
+        else if ("MouseAxisWheel".equals(string)) {
+            zoomCamera(.1f);
+        }
+        else if ("MouseAxisWheel-".equals(string)) {
+            zoomCamera(-.1f);
+        }
     }
-
-    public void onPostUpdate(float f) {
-    }
-
 }

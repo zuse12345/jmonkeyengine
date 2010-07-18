b5572c4ce58f3976d3ca49d6e722736bd1a26a59
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-07-18 22:26:54 +0000

    jme3: - improve some physics tests

--- branches/jme3/src/test/jme3test/bullet/TestCollisionGroups.java
+++ branches/jme3/src/test/jme3test/bullet/TestCollisionGroups.java
@@ -6,21 +6,16 @@
 package jme3test.bullet;
 
 import com.jme3.app.SimpleBulletApplication;
-import com.jme3.asset.TextureKey;
-import com.jme3.bounding.BoundingBox;
-import com.jme3.bullet.collision.shapes.BoxCollisionShape;
-import com.jme3.bullet.collision.shapes.CylinderCollisionShape;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.nodes.PhysicsNode;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
-import com.jme3.scene.shape.Cylinder;
 import com.jme3.scene.shape.Sphere;
-import com.jme3.texture.Texture;
 
 /**
  *
@@ -35,31 +30,22 @@ public class TestCollisionGroups extends SimpleBulletApplication{
 
     @Override
     public void simpleInitApp() {
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/SimpleTextured.j3md");
-        TextureKey key = new TextureKey("Interface/Logo/Monkey.jpg", true);
-        key.setGenerateMips(true);
-        Texture tex = assetManager.loadTexture(key);
-        tex.setMinFilter(Texture.MinFilter.Trilinear);
-        mat.setTexture("m_ColorMap", tex);
+        Material mat = new Material(getAssetManager(), "Common/MatDefs/Misc/WireColor.j3md");
+        mat.setColor("m_Color", ColorRGBA.Blue);
 
 //         Add a physics sphere to the world
-        Sphere sphere=new Sphere(16,16,1f);
-        Geometry geom=new Geometry("sphere",sphere);
-        geom.setMaterial(mat);
-        PhysicsNode physicsSphere=new PhysicsNode(geom,new SphereCollisionShape(1),1);
+        PhysicsNode physicsSphere=new PhysicsNode(new SphereCollisionShape(1),1);
         physicsSphere.setLocalTranslation(new Vector3f(3,6,0));
+        physicsSphere.attachDebugShape(assetManager);
         physicsSphere.updateGeometricState();
         physicsSphere.updateModelBound();
         rootNode.attachChild(physicsSphere);
         getPhysicsSpace().add(physicsSphere);
 
         // Add a physics sphere to the world using the collision shape from sphere one
-        Sphere sphere2=new Sphere(16,16,1f);
-        Geometry geom2=new Geometry("sphere2",sphere2);
-        geom2.setMaterial(mat);
-        PhysicsNode physicsSphere2=new PhysicsNode(geom2,physicsSphere.getCollisionShape(),1);
+        PhysicsNode physicsSphere2=new PhysicsNode(physicsSphere.getCollisionShape(),1);
         physicsSphere2.setLocalTranslation(new Vector3f(4,8,0));
+        physicsSphere2.attachDebugShape(assetManager);
         physicsSphere2.updateGeometricState();
         physicsSphere2.updateModelBound();
         physicsSphere2.addCollideWithGroup(PhysicsNode.COLLISION_GROUP_02);
--- branches/jme3/src/test/jme3test/bullet/TestGhostObject.java
+++ branches/jme3/src/test/jme3test/bullet/TestGhostObject.java
@@ -2,16 +2,14 @@ package jme3test.bullet;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleBulletApplication;
-import com.jme3.asset.TextureKey;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.nodes.PhysicsGhostNode;
 import com.jme3.bullet.nodes.PhysicsNode;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
 import java.util.logging.Logger;
 
 /**
@@ -29,12 +27,10 @@ public class TestGhostObject extends SimpleBulletApplication {
 
     private void initGhostObject() {
         Vector3f halfExtents = new Vector3f(3, 4.2f, 1);
-        // create an visual Box to see, where the GhostObject is:
-        Box boxGeom = new Box(Vector3f.ZERO,
-                halfExtents.x, halfExtents.y, halfExtents.z);
-        Geometry ghostGeom = new Geometry("ghostVisual", boxGeom);
-        ghostGeom.setMaterial(assetManager.loadMaterial("Common/Materials/RedColor.j3m"));
-        ghostNode = new PhysicsGhostNode(ghostGeom, new BoxCollisionShape(halfExtents));
+        Material mat = new Material(getAssetManager(), "Common/MatDefs/Misc/WireColor.j3md");
+        mat.setColor("m_Color", ColorRGBA.Red);
+        ghostNode = new PhysicsGhostNode(new BoxCollisionShape(halfExtents));
+        ghostNode.attachDebugShape(mat);
         rootNode.attachChild(ghostNode);
         getPhysicsSpace().add(ghostNode);
     }
@@ -42,59 +38,47 @@ public class TestGhostObject extends SimpleBulletApplication {
     @Override
     public void simpleInitApp() {
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/SimpleTextured.j3md");
-        TextureKey key = new TextureKey("Interface/Logo/Monkey.jpg", true);
-        key.setGenerateMips(true);
-        Texture tex = assetManager.loadTexture(key);
-        tex.setMinFilter(Texture.MinFilter.Trilinear);
-        mat.setTexture("m_ColorMap", tex);
-
         // Mesh to be shared across several boxes.
         Box boxGeom = new Box(Vector3f.ZERO, 1f, 1f, 1f);
         // CollisionShape to be shared across several boxes.
         CollisionShape shape = new BoxCollisionShape(new Vector3f(1, 1, 1));
 
         // Add some phyisics boxes higher up, to fall down and be tracked.
-        Geometry geom0 = new Geometry("box", boxGeom);
-        geom0.setMaterial(mat);
-        PhysicsNode physicsBox = new PhysicsNode(geom0, new BoxCollisionShape(new Vector3f(1, 1, 1)), 1);
+        PhysicsNode physicsBox = new PhysicsNode(new BoxCollisionShape(new Vector3f(1, 1, 1)), 1);
         physicsBox.setName("box0");
         physicsBox.setFriction(0.1f);
         physicsBox.setLocalTranslation(new Vector3f(.6f, 4, .5f));
+        physicsBox.attachDebugShape(assetManager);
         physicsBox.updateGeometricState();
         physicsBox.updateModelBound();
         rootNode.attachChild(physicsBox);
         getPhysicsSpace().add(physicsBox);
 
-        Geometry geom1 = new Geometry("box", boxGeom);
-        geom1.setMaterial(mat);
-        PhysicsNode physicsBox1 = new PhysicsNode(geom1, shape, 1);
+        PhysicsNode physicsBox1 = new PhysicsNode(shape, 1);
         physicsBox1.setName("box1");
         physicsBox1.setFriction(0.1f);
         physicsBox1.setLocalTranslation(new Vector3f(0, 40, 0));
+        physicsBox1.attachDebugShape(assetManager);
         physicsBox1.updateGeometricState();
         physicsBox1.updateModelBound();
         rootNode.attachChild(physicsBox1);
         getPhysicsSpace().add(physicsBox1);
 
-        Geometry geom2 = new Geometry("box", boxGeom);
-        geom2.setMaterial(mat);
-        PhysicsNode physicsBox2 = new PhysicsNode(geom2, new BoxCollisionShape(new Vector3f(1, 1, 1)), 1);
+        PhysicsNode physicsBox2 = new PhysicsNode(new BoxCollisionShape(new Vector3f(1, 1, 1)), 1);
         physicsBox2.setName("box0");
         physicsBox2.setFriction(0.1f);
         physicsBox2.setLocalTranslation(new Vector3f(.5f, 80, -.8f));
+        physicsBox2.attachDebugShape(assetManager);
         physicsBox2.updateGeometricState();
         physicsBox2.updateModelBound();
         rootNode.attachChild(physicsBox2);
         getPhysicsSpace().add(physicsBox2);
 
         // the floor, does not move (mass=0)
-        Geometry geom3 = new Geometry("box2", new Box(Vector3f.ZERO, 100f, 1f, 100f));
-        geom3.setMaterial(mat);
-        geom3.updateGeometricState();
-        PhysicsNode node = new PhysicsNode(geom3, new BoxCollisionShape(new Vector3f(100, 1, 100)), 0);
+        PhysicsNode node = new PhysicsNode(new BoxCollisionShape(new Vector3f(100, 1, 100)), 0);
         node.setName("floor");
         node.setLocalTranslation(new Vector3f(0f, -6, 0f));
+        node.attachDebugShape(assetManager);
         rootNode.attachChild(node);
         node.updateModelBound();
         node.updateGeometricState();

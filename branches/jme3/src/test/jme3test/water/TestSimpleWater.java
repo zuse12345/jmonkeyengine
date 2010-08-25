6fdb9227eae29f0795608dc91fad8c96f1b185ac
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-08-25 12:28:24 +0000

    jme3: - use separate scene in water shader test

--- branches/jme3/src/test/jme3test/water/TestSimpleWater.java
+++ branches/jme3/src/test/jme3test/water/TestSimpleWater.java
@@ -10,6 +10,7 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 import com.jme3.water.SimpleWaterProcessor;
@@ -27,21 +28,30 @@ public class TestSimpleWater extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        //init cam location
+        cam.setLocation(new Vector3f(0,10,10));
+        cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
+
+        //init scene
+        Node sceneNode=new Node("Scene");
         Box b = new Box(Vector3f.ZERO, 1, 1, 1);
         Geometry geom = new Geometry("Box", b);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/SimpleTextured.j3md");
         mat.setTexture("m_ColorMap", assetManager.loadTexture("Interface/Logo/Monkey.jpg"));
         geom.setMaterial(mat);
-        rootNode.attachChild(geom);
+        sceneNode.attachChild(geom);
+        rootNode.attachChild(sceneNode);
 
+        //create processor
         SimpleWaterProcessor waterProcessor = new SimpleWaterProcessor(assetManager);
-        waterProcessor.setReflectionScene(rootNode);
+        waterProcessor.setReflectionScene(sceneNode);
 
+        //create water quad
         Quad quad = new Quad(10, 10);
         Geometry waterPlane = new Geometry("WaterPlane", quad);
-//        waterPlane.setLocalRotation(new Quaternion().fromAngleAxis(-FastMath.HALF_PI, Vector3f.UNIT_X));
+        waterPlane.setLocalRotation(new Quaternion().fromAngleAxis(-FastMath.HALF_PI, Vector3f.UNIT_X));
         waterPlane.setMaterial(waterProcessor.getMaterial());
-//        waterPlane.setLocalTranslation(-5, 0, 5);
+        waterPlane.setLocalTranslation(-5, 0, 5);
 
         rootNode.attachChild(waterPlane);
 

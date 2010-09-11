42409f0697d44460bb8d06ece2bef3c1d036d8ea
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-09-11 00:10:42 +0000

    jme3: - fix scene ogre conversion

--- branches/jme3-gde/jme3-ogrexml/src/com/jme3/gde/ogrexml/OgreSceneDataObject.java
+++ branches/jme3-gde/jme3-ogrexml/src/com/jme3/gde/ogrexml/OgreSceneDataObject.java
@@ -31,14 +31,18 @@
  */
 package com.jme3.gde.ogrexml;
 
+import com.jme3.gde.core.assets.ProjectAssetManager;
 import com.jme3.gde.core.assets.SpatialAssetDataObject;
+import com.jme3.scene.Spatial;
 import java.io.IOException;
+import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiFileLoader;
 import org.openide.nodes.CookieSet;
 import org.openide.nodes.Node;
 import org.openide.text.DataEditorSupport;
+import org.openide.util.Exceptions;
 
 public class OgreSceneDataObject extends SpatialAssetDataObject {
 
@@ -46,6 +50,25 @@ public class OgreSceneDataObject extends SpatialAssetDataObject {
         super(pf, loader);
         CookieSet cookies = getCookieSet();
         cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
+    }
+
+    @Override
+    public Spatial loadAsset() {
+        ProjectAssetManager mgr=getLookup().lookup(ProjectAssetManager.class);
+        String assetKey = mgr.getRelativeAssetPath(getPrimaryFile().getPath());
+        FileLock lock = null;
+        try {
+            lock = getPrimaryFile().lock();
+            Spatial spatial = mgr.getManager().loadModel(assetKey);
+            lock.releaseLock();
+            return spatial;
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+            if (lock != null) {
+                lock.releaseLock();
+            }
+        }
+        return null;
     }
 
 }

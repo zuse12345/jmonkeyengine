57a96cf78ac8004b3446f77065e5e81c730cfb8f
remy.bouquet@gmail.com <remy.bouquet@gmail.com@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-09-18 13:33:05 +0000

    Chase camera : - Chase camera now  implements control, so it updates on its own - Changed the test so the target moves with arrow keys - added javadoc for the chase camera

--- branches/jme3/src/core/com/jme3/input/ChaseCamera.java
+++ branches/jme3/src/core/com/jme3/input/ChaseCamera.java
@@ -1,5 +1,9 @@
 package com.jme3.input;
 
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.MouseAxisTrigger;
@@ -7,19 +11,26 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.Control;
+import java.io.IOException;
 
-public class ChaseCamera implements ActionListener, AnalogListener {
+/**
+ * A camera that follows a spatial and can turn around it by dragging the mouse
+ * @author nehon
+ */
+public class ChaseCamera implements ActionListener, AnalogListener, Control {
 
     private Spatial target = null;
     private float distance = 20;
-    private float minHeight = 0.05f;
+    private float minHeight = 0.00f;
     private float maxHeight = FastMath.PI / 2;
     private float minDistance = 1.0f;
     private float maxDistance = 40.0f;
     private float zoomSpeed = 2.0f;
     private float rotationSpeed = 1.0f;
-
     /**
      * the camera.
      */
@@ -29,13 +40,32 @@ public class ChaseCamera implements ActionListener, AnalogListener {
     private boolean canRotate;
     private float rotation = FastMath.PI / 2;
     private float vRotation = 0;
-
     private boolean enabled = true;
 
+    /**
+     * Constructs the chase camera
+     * @param cam the application camera
+     * @param target the spatial to follow
+     */
     public ChaseCamera(Camera cam, final Spatial target) {
         this.target = target;
+        target.addControl(this);
         this.cam = cam;
         initialUpVec = cam.getUp().clone();
+    }
+
+    /**
+     * Constructs the chase camera, and registers inputs
+     * @param cam the application camera
+     * @param target the spatial to follow
+     * @param inputManager the inputManager of the application to register inputs
+     */
+    public ChaseCamera(Camera cam, final Spatial target, InputManager inputManager) {
+        this.target = target;
+        target.addControl(this);
+        this.cam = cam;
+        initialUpVec = cam.getUp().clone();
+        registerWithInput(inputManager);
     }
 
     public void onAction(String name, boolean keyPressed, float tpf) {
@@ -48,6 +78,7 @@ public class ChaseCamera implements ActionListener, AnalogListener {
                 inputManager.setCursorVisible(true);
             }
         }
+
     }
 
     public void onAnalog(String name, float value, float tpf) {
@@ -64,19 +95,24 @@ public class ChaseCamera implements ActionListener, AnalogListener {
         } else if (name.equals("ZoomOut")) {
             zoomCamera(-value);
         }
+
     }
 
+    /**
+     * Registers inputs with the input manager
+     * @param inputManager
+     */
     public void registerWithInput(InputManager inputManager) {
         String[] inputs = {"toggleRotate", "Down", "Up", "mouseLeft", "mouseRight", "ZoomIn", "ZoomOut"};
 
         this.inputManager = inputManager;
 
-        inputManager.addMapping("Down",         new MouseAxisTrigger(1, true));
-        inputManager.addMapping("Up",           new MouseAxisTrigger(1, false));
-        inputManager.addMapping("ZoomIn",       new MouseAxisTrigger(2, true));
-        inputManager.addMapping("ZoomOut",      new MouseAxisTrigger(2, false));
-        inputManager.addMapping("mouseLeft",    new MouseAxisTrigger(0, true));
-        inputManager.addMapping("mouseRight",   new MouseAxisTrigger(0, false));
+        inputManager.addMapping("Down", new MouseAxisTrigger(1, true));
+        inputManager.addMapping("Up", new MouseAxisTrigger(1, false));
+        inputManager.addMapping("ZoomIn", new MouseAxisTrigger(2, true));
+        inputManager.addMapping("ZoomOut", new MouseAxisTrigger(2, false));
+        inputManager.addMapping("mouseLeft", new MouseAxisTrigger(0, true));
+        inputManager.addMapping("mouseRight", new MouseAxisTrigger(0, false));
         inputManager.addMapping("toggleRotate", new MouseButtonTrigger(0));
         inputManager.addMapping("toggleRotate", new MouseButtonTrigger(1));
 
@@ -85,18 +121,20 @@ public class ChaseCamera implements ActionListener, AnalogListener {
         updateCamera();
     }
 
+    //rotate the camera around the target on the horizontal plane
     private void rotateCamera(float value) {
         if (!canRotate || !enabled) {
             return;
         }
-        
         rotation += value * rotationSpeed;
         updateCamera();
     }
 
+    //move the camera toward or away the target
     private void zoomCamera(float value) {
-        if (!enabled)
+        if (!enabled) {
             return;
+        }
 
         distance += value * zoomSpeed;
         if (distance > maxDistance) {
@@ -112,6 +150,7 @@ public class ChaseCamera implements ActionListener, AnalogListener {
         updateCamera();
     }
 
+    //rotate the camera around the target on the vertical plane
     private void vRotateCamera(float value) {
         if (!canRotate || !enabled) {
             return;
@@ -126,6 +165,9 @@ public class ChaseCamera implements ActionListener, AnalogListener {
         updateCamera();
     }
 
+    /**
+     * Update the camera, should only be called internally
+     */
     public void updateCamera() {
         float hDistance = distance * FastMath.sin((FastMath.PI / 2) - vRotation);
         Vector3f pos = new Vector3f(hDistance * FastMath.cos(rotation), distance * FastMath.sin(vRotation), hDistance * FastMath.sin(rotation));
@@ -134,30 +176,113 @@ public class ChaseCamera implements ActionListener, AnalogListener {
         cam.lookAt(target.getLocalTranslation(), initialUpVec);
     }
 
+    /**
+     * Return the enabled/disabled state of the camera
+     * @return true if the camera is enabled
+     */
     public boolean isEnabled() {
         return enabled;
     }
 
+    /**
+     * Enable or disable the camera
+     * @param enabled true to enable
+     */
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
-        if (!enabled)
+        if (!enabled) {
             canRotate = false; // reset this flag in-case it was on before
+        }
     }
-    
+
+    /**
+     * Returns the max zoom distance of the camera (default is 40)
+     * @return maxDistance
+     */
     public float getMaxDistance() {
         return maxDistance;
     }
 
+    /**
+     * Sets the max zoom distance of the camera (default is 40)
+     * @param maxDistance
+     */
     public void setMaxDistance(float maxDistance) {
         this.maxDistance = maxDistance;
     }
 
+    /**
+     * Returns the min zoom distance of the camera (default is 1)
+     * @return minDistance
+     */
     public float getMinDistance() {
         return minDistance;
     }
 
+    /**
+     * Sets the min zoom distance of the camera (default is 1)
+     * @return minDistance
+     */
     public void setMinDistance(float minDistance) {
         this.minDistance = minDistance;
     }
 
+    /**
+     * clone this camera for a spatial
+     * @param spatial
+     * @return
+     */
+    public Control cloneForSpatial(Spatial spatial) {
+        ChaseCamera cc = new ChaseCamera(cam, spatial, inputManager);
+        cc.setMaxDistance(getMaxDistance());
+        cc.setMinDistance(getMinDistance());
+        return cc;
+    }
+
+    /**
+     * Sets the spacial for the camera control, should only be used internally
+     * @param spatial
+     */
+    public void setSpatial(Spatial spatial) {
+        target = spatial;
+    }
+
+    /**
+     * update the camera control, should on ly be used internally
+     * @param tpf
+     */
+    public void update(float tpf) {
+        updateCamera();
+    }
+
+    /**
+     * renders the camera control, should on ly be used internally
+     * @param rm
+     * @param vp
+     */
+    public void render(RenderManager rm, ViewPort vp) {
+        //nothing to render
+    }
+
+    /**
+     * Write the camera
+     * @param ex the exporter
+     * @throws IOException
+     */
+    public void write(JmeExporter ex) throws IOException {
+        OutputCapsule capsule = ex.getCapsule(this);
+        capsule.write(maxDistance, "maxDistance", 40);
+        capsule.write(minDistance, "minDistance", 1);
+    }
+
+    /**
+     * Read the camera
+     * @param im
+     * @throws IOException
+     */
+    public void read(JmeImporter im) throws IOException {
+        InputCapsule ic = im.getCapsule(this);
+        maxDistance = ic.readFloat("maxDistance", 40);
+        minDistance = ic.readFloat("minDistance", 1);
+    }
 }
--- branches/jme3/src/test/jme3test/input/TestChaseCamera.java
+++ branches/jme3/src/test/jme3test/input/TestChaseCamera.java
@@ -2,14 +2,23 @@ package jme3test.input;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.input.ChaseCamera;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
+import com.jme3.math.FastMath;
+import com.jme3.math.Plane;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Quad;
 
 /**
  * Tests OBJ format loading
  */
-public class TestChaseCamera extends SimpleApplication {
+public class TestChaseCamera extends SimpleApplication implements AnalogListener {
 
+    private Geometry teaGeom;
+    private ChaseCamera chaseCam;
     public static void main(String[] args){
         TestChaseCamera app = new TestChaseCamera();
         app.start();
@@ -17,18 +26,50 @@ public class TestChaseCamera extends SimpleApplication {
 
     public void simpleInitApp() {
         // Load a teapot model
-        Geometry teaGeom = (Geometry) assetManager.loadModel("Models/Teapot/Teapot.obj");
+        teaGeom = (Geometry) assetManager.loadModel("Models/Teapot/Teapot.obj");
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
         teaGeom.setMaterial(mat);
         rootNode.attachChild(teaGeom);
-
+        mat = new Material(assetManager, "Common/MatDefs/Misc/SimpleTextured.j3md");
+        mat.setTexture("m_ColorMap", assetManager.loadTexture("Interface/Logo/Monkey.jpg"));
+        Geometry ground=new Geometry("ground", new Quad(50, 50));
+        ground.setLocalRotation(new Quaternion().fromAngleAxis(-FastMath.HALF_PI, Vector3f.UNIT_X));
+        ground.setLocalTranslation(-25, -1, 25);
+        ground.setMaterial(mat);
+        rootNode.attachChild(ground);
         // Disable the flyby cam
         flyCam.setEnabled(false);
 
         // Enable a chase cam
-        ChaseCamera chaseCam = new ChaseCamera(cam, teaGeom);
-        chaseCam.setMaxDistance(3);
-        chaseCam.setMinDistance(1);
-        chaseCam.registerWithInput(inputManager);
+        chaseCam = new ChaseCamera(cam, teaGeom,inputManager);
+        regsiterInput();
     }
+
+    public void regsiterInput(){
+        inputManager.addMapping("moveForward",new KeyTrigger(keyInput.KEY_UP));
+        inputManager.addMapping("moveBackward",new KeyTrigger(keyInput.KEY_DOWN));
+        inputManager.addMapping("moveRight",new KeyTrigger(keyInput.KEY_RIGHT));
+        inputManager.addMapping("moveLeft",new KeyTrigger(keyInput.KEY_LEFT));
+        inputManager.addListener(this, "moveForward","moveBackward","moveRight","moveLeft");
+    }
+
+    public void onAnalog(String name, float value, float tpf) {
+        if(name.equals("moveForward")){
+            teaGeom.move(0, 0, -5*tpf);
+        }
+        if(name.equals("moveBackward")){
+            teaGeom.move(0, 0, 5*tpf);
+        }
+        if(name.equals("moveRight")){
+            teaGeom.move(5*tpf, 0, 0);
+        }
+        if(name.equals("moveLeft")){
+            teaGeom.move(-5*tpf, 0, 0);
+        }
+
+    }
+
+    
+
+
 }

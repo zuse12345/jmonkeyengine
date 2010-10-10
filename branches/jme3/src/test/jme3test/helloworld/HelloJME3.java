0b11bb654a79685cb7a269273039cd178bd85652
rootli <rootli@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-10-10 14:28:39 +0000

    rephrased comments in tutorial code

--- branches/jme3/src/test/jme3test/helloworld/HelloJME3.java
+++ branches/jme3/src/test/jme3test/helloworld/HelloJME3.java
@@ -33,6 +33,7 @@
 package jme3test.helloworld;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.FlyByCamera;
 import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
@@ -51,12 +52,12 @@ public class HelloJME3 extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);  // take a cube shape
-        Geometry geom = new Geometry("Box", b);   // create a Spatial from it
-        Material mat = new Material(assetManager, 
-          "Common/MatDefs/Misc/SolidColor.j3md"); // load a solid color material
-        mat.setColor("m_Color", ColorRGBA.Blue);  // make the solid color blue
-        geom.setMaterial(mat);      // give the box the solid blue material
-        rootNode.attachChild(geom); // make the blue box appear in the scene
+        Box b = new Box(Vector3f.ZERO, 1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+         "Common/MatDefs/Misc/SolidColor.j3md"); // create a simple material
+        mat.setColor("m_Color", ColorRGBA.Blue); // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
     }
 }

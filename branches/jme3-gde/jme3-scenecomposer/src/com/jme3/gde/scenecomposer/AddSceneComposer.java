636d6f65fb46e1390237f8a501eec60e603145ff
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-10-18 11:59:04 +0000

    jme3-gde: - Allow adding/linking multiple models at once to SceneComposer

--- branches/jme3-gde/jme3-scenecomposer/src/com/jme3/gde/scenecomposer/AddSceneComposer.java
+++ branches/jme3-gde/jme3-scenecomposer/src/com/jme3/gde/scenecomposer/AddSceneComposer.java
@@ -8,19 +8,24 @@ import com.jme3.gde.core.assets.ProjectAssetManager;
 import com.jme3.gde.core.assets.SpatialAssetDataObject;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Iterator;
+import java.util.List;
 
 public final class AddSceneComposer implements ActionListener {
 
-    private final SpatialAssetDataObject context;
+    private final List<SpatialAssetDataObject> context;
 
-    public AddSceneComposer(SpatialAssetDataObject context) {
+    public AddSceneComposer(List<SpatialAssetDataObject> context) {
         this.context = context;
     }
 
     public void actionPerformed(ActionEvent ev) {
-        ProjectAssetManager manager=context.getLookup().lookup(ProjectAssetManager.class);
-        if(manager == null) return;
-        SceneComposerTopComponent composer=SceneComposerTopComponent.findInstance();
-        composer.addModel(context);
+        for (Iterator<SpatialAssetDataObject> it = context.iterator(); it.hasNext();) {
+            SpatialAssetDataObject spatialAssetDataObject = it.next();
+            ProjectAssetManager manager=spatialAssetDataObject.getLookup().lookup(ProjectAssetManager.class);
+            if(manager == null) return;
+            SceneComposerTopComponent composer=SceneComposerTopComponent.findInstance();
+            composer.addModel(spatialAssetDataObject);
+        }
     }
 }
--- branches/jme3-gde/jme3-scenecomposer/src/com/jme3/gde/scenecomposer/LinkSceneComposer.java
+++ branches/jme3-gde/jme3-scenecomposer/src/com/jme3/gde/scenecomposer/LinkSceneComposer.java
@@ -8,22 +8,29 @@ import com.jme3.gde.core.assets.ProjectAssetManager;
 import com.jme3.gde.core.assets.SpatialAssetDataObject;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Iterator;
+import java.util.List;
 import org.openide.filesystems.FileObject;
 
 public final class LinkSceneComposer implements ActionListener {
 
-    private final SpatialAssetDataObject context;
+    private final List<SpatialAssetDataObject> context;
 
-    public LinkSceneComposer(SpatialAssetDataObject context) {
+    public LinkSceneComposer(List<SpatialAssetDataObject> context) {
         this.context = context;
     }
 
     public void actionPerformed(ActionEvent ev) {
-        ProjectAssetManager manager=context.getLookup().lookup(ProjectAssetManager.class);
-        if(manager == null) return;
-        FileObject file=context.getPrimaryFile();
-        String assetName=manager.getRelativeAssetPath(file.getPath());
-        SceneComposerTopComponent composer=SceneComposerTopComponent.findInstance();
-        composer.linkModel(manager.getManager(),assetName);
+        for (Iterator<SpatialAssetDataObject> it = context.iterator(); it.hasNext();) {
+            SpatialAssetDataObject spatialAssetDataObject = it.next();
+            ProjectAssetManager manager = spatialAssetDataObject.getLookup().lookup(ProjectAssetManager.class);
+            if (manager == null) {
+                return;
+            }
+            FileObject file = spatialAssetDataObject.getPrimaryFile();
+            String assetName = manager.getRelativeAssetPath(file.getPath());
+            SceneComposerTopComponent composer = SceneComposerTopComponent.findInstance();
+            composer.linkModel(manager.getManager(), assetName);
+        }
     }
 }

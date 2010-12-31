e6d11be5c259478116b8041db62dcf9bdee4b6c2
shadowislord <shadowislord@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-12-31 20:27:47 +0000

    * Added sky from 6 textures option to SkyFactory  * Created test for it as well

--- branches/jme3/src/core/com/jme3/util/SkyFactory.java
+++ branches/jme3/src/core/com/jme3/util/SkyFactory.java
@@ -8,7 +8,9 @@ import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Sphere;
+import com.jme3.texture.Image;
 import com.jme3.texture.Texture;
+import com.jme3.texture.TextureCubeMap;
 
 public class SkyFactory {
 
@@ -27,7 +29,44 @@ public class SkyFactory {
         }
         sky.setMaterial(skyMat);
         
+        return sky;
+    }
+
+    public static Spatial createSky(AssetManager assetManager, Texture west, Texture east, Texture north, Texture south, Texture up, Texture down, Vector3f normalScale){
+        Geometry sky = new Geometry("Sky", sphereMesh);
+        sky.setQueueBucket(Bucket.Sky);
+        sky.setCullHint(Spatial.CullHint.Never);
+
+        Image westImg  = west.getImage();
+        Image eastImg  = east.getImage();
+        Image northImg = north.getImage();
+        Image southImg = south.getImage();
+        Image upImg    = up.getImage();
+        Image downImg  = down.getImage();
+
+        Image cubeImage = new Image(westImg.getFormat(), westImg.getWidth(), westImg.getHeight(), null);
         
+        cubeImage.addData(westImg.getData(0));
+        cubeImage.addData(eastImg.getData(0));
+        
+        cubeImage.addData(downImg.getData(0));
+        cubeImage.addData(upImg.getData(0));
+
+        
+        cubeImage.addData(southImg.getData(0));
+        cubeImage.addData(northImg.getData(0));
+
+        TextureCubeMap cubeMap = new TextureCubeMap(cubeImage);
+        cubeMap.setAnisotropicFilter(0);
+        cubeMap.setMagFilter(Texture.MagFilter.Bilinear);
+        cubeMap.setMinFilter(Texture.MinFilter.NearestNoMipMaps);
+        cubeMap.setWrap(Texture.WrapMode.EdgeClamp);
+
+        Material skyMat = new Material(assetManager, "Common/MatDefs/Misc/Sky.j3md");
+        skyMat.setTexture("m_Texture", cubeMap);
+        skyMat.setVector3("m_NormalScale", normalScale);
+        sky.setMaterial(skyMat);
+
         return sky;
     }
 
@@ -42,4 +81,7 @@ public class SkyFactory {
         Texture tex = assetManager.loadTexture(key);
         return createSky(assetManager, tex, sphereMap);
     }
+
+    
+
 }

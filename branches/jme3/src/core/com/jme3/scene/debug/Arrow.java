78c111ab5eb241eccda5890bf02ff8dbee7cbdd9
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-01-17 01:36:56 +0000

    jme3: - allow changing of Arrow mesh

--- branches/jme3/src/core/com/jme3/scene/debug/Arrow.java
+++ branches/jme3/src/core/com/jme3/scene/debug/Arrow.java
@@ -29,49 +29,48 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package com.jme3.scene.debug;
 
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer.Type;
-import com.jme3.util.TempVars;
+import java.nio.FloatBuffer;
 
 public class Arrow extends Mesh {
+    Quaternion tempQuat = new Quaternion();
+    Vector3f tempVec = new Vector3f();
 
     private static final float[] positions = new float[]{
-                    0,     0,      0,
-                    0,     0,      1, // tip
-                    0.05f,  0,   0.9f, // tip right
-                   -0.05f,  0,   0.9f, // tip left
-                    0,   0.05f,  0.9f, // tip top
-                    0,  -0.05f,  0.9f, // tip buttom
+        0, 0, 0,
+        0, 0, 1, // tip
+        0.05f, 0, 0.9f, // tip right
+        -0.05f, 0, 0.9f, // tip left
+        0, 0.05f, 0.9f, // tip top
+        0, -0.05f, 0.9f, // tip buttom
     };
 
     public Arrow() {
     }
 
-    public Arrow(Vector3f extent){
+    public Arrow(Vector3f extent) {
         float len = extent.length();
         Vector3f dir = extent.normalize();
 
-        Quaternion tempQuat = new Quaternion();
         tempQuat.lookAt(dir, Vector3f.UNIT_Y);
         tempQuat.normalize();
-        Vector3f tempVec = new Vector3f();
 
         float[] newPositions = new float[positions.length];
-        for (int i = 0; i < positions.length; i+=3){
+        for (int i = 0; i < positions.length; i += 3) {
             Vector3f vec = tempVec.set(positions[i],
-                                       positions[i+1],
-                                       positions[i+2]);
+                    positions[i + 1],
+                    positions[i + 2]);
             vec.multLocal(len);
             tempQuat.mult(vec, vec);
 
-            newPositions[i]   = vec.getX();
-            newPositions[i+1] = vec.getY();
-            newPositions[i+2] = vec.getZ();
+            newPositions[i] = vec.getX();
+            newPositions[i + 1] = vec.getY();
+            newPositions[i + 2] = vec.getZ();
         }
 
         setBuffer(Type.Position, 3, newPositions);
@@ -81,10 +80,33 @@ public class Arrow extends Mesh {
                     1, 2,
                     1, 3,
                     1, 4,
-                    1, 5,
-                }
-        );
+                    1, 5,});
         setMode(Mode.Lines);
+
+        updateBound();
+        updateCounts();
+    }
+
+    public void setArrowExtent(Vector3f extent) {
+        float len = extent.length();
+//        Vector3f dir = extent.normalize();
+
+        tempQuat.lookAt(extent, Vector3f.UNIT_Y);
+        tempQuat.normalize();
+
+        FloatBuffer buffer = getFloatBuffer(Type.Position);
+        buffer.rewind();
+        for (int i = 0; i < positions.length; i += 3) {
+            Vector3f vec = tempVec.set(positions[i],
+                    positions[i + 1],
+                    positions[i + 2]);
+            vec.multLocal(len);
+            tempQuat.mult(vec, vec);
+
+            buffer.put(vec.x);
+            buffer.put(vec.y);
+            buffer.put(vec.z);
+        }
 
         updateBound();
         updateCounts();

f8f8eadd82325335dbc80d96ce668734413415ab
mulova <mulova@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-02-17 13:19:36 +0000

    remove unused import

--- branches/jme3/src/core/com/jme3/font/BitmapCharacterSet.java
+++ branches/jme3/src/core/com/jme3/font/BitmapCharacterSet.java
@@ -32,16 +32,15 @@
 
 package com.jme3.font;
 
+import java.io.IOException;
+
+import com.jme3.export.InputCapsule;
 import com.jme3.export.JmeExporter;
 import com.jme3.export.JmeImporter;
-import com.jme3.export.InputCapsule;
 import com.jme3.export.OutputCapsule;
 import com.jme3.export.Savable;
 import com.jme3.util.IntMap;
 import com.jme3.util.IntMap.Entry;
-import com.sun.xml.internal.fastinfoset.stax.events.CharactersEvent;
-
-import java.io.IOException;
 
 public class BitmapCharacterSet implements Savable {
 

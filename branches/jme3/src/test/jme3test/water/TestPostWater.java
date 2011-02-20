2a650e9d63af4aea5e5647821381f9b6fc3b55fa
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-02-20 17:31:44 +0000

    jme3: - use terrain in post water test

--- branches/jme3/src/test/jme3test/water/TestPostWater.java
+++ branches/jme3/src/test/jme3test/water/TestPostWater.java
@@ -2,23 +2,29 @@ package jme3test.water;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.audio.AudioNode;
+import com.jme3.bounding.BoundingBox;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
-import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 
 import com.jme3.post.FilterPostProcessor;
+import com.jme3.renderer.Camera;
 import com.jme3.renderer.queue.RenderQueue.ShadowMode;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.terrain.geomipmap.TerrainQuad;
+import com.jme3.terrain.heightmap.AbstractHeightMap;
+import com.jme3.terrain.heightmap.ImageBasedHeightMap;
+import com.jme3.texture.Texture;
 import com.jme3.texture.Texture.WrapMode;
 import com.jme3.util.SkyFactory;
-import com.jme3.util.TangentBinormalGenerator;
 import com.jme3.water.WaterFilter;
+import java.util.ArrayList;
+import java.util.List;
+import jme3tools.converters.ImageToAwt;
 
 /**
  * test
@@ -29,9 +35,12 @@ public class TestPostWater extends SimpleApplication {
     private FilterPostProcessor fpp;
     private Vector3f lightDir = new Vector3f(-4.9236743f, -1.27054665f, 5.896916f);
     private WaterFilter water;
+    TerrainQuad terrain;
+    Material matRock;
+    Material matWire;
 
     public static void main(String[] args) {
-        TestPostWater app = new TestPostWater();        
+        TestPostWater app = new TestPostWater();
         app.start();
     }
 
@@ -39,32 +48,10 @@ public class TestPostWater extends SimpleApplication {
     public void simpleInitApp() {
 
 
-//        Material mat = assetManager.loadMaterial("Textures/Terrain/Rocky/Rocky.j3m");
-//        Spatial scene = assetManager.loadModel("Models/Terrain/Terrain.mesh.xml");
-//        TangentBinormalGenerator.generate(((Geometry)((Node)scene).getChild(0)).getMesh());
-//        scene.setMaterial(mat);
-//        scene.setShadowMode(ShadowMode.CastAndReceive);
-//        scene.setLocalScale(400);
-//        scene.setLocalTranslation(0, -10, -120);
-//        Node mainScene =new Node();
-//        mainScene.attachChild(scene);
-
-        Node mainScene = (Node) assetManager.loadModel("/Scenes/Beach/Beach2.mesh.j3o");
-        Geometry beach = (Geometry) mainScene.getChild("Beach2-geom-1");
-
-        beach.setLocalScale(10);
-        beach.setLocalRotation(new Quaternion().fromAngleAxis(FastMath.PI, Vector3f.UNIT_Y));
-        beach.setShadowMode(ShadowMode.CastAndReceive);
-
-        Material mat = assetManager.loadMaterial("/Scenes/Beach/sand.j3m");
-        mat.getTextureParam("DiffuseMap").getTextureValue().setWrap(WrapMode.Repeat);
-        mat.getTextureParam("NormalMap").getTextureValue().setWrap(WrapMode.Repeat);
-
-        beach.setMaterial(mat);
-
-        beach.getMesh().scaleTextureCoordinates(new Vector2f(60f, 60f));
-        TangentBinormalGenerator.generate(beach.getMesh());
+        Node mainScene = new Node("Main Scene");
         rootNode.attachChild(mainScene);
+
+        createTerrain(mainScene);
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(lightDir);
         sun.setColor(ColorRGBA.White.clone().multLocal(1.7f));
@@ -77,8 +64,8 @@ public class TestPostWater extends SimpleApplication {
 
         flyCam.setMoveSpeed(50);
 
-        cam.setLocation(new Vector3f(-3.613934f, 33.164284f, 106.1938f));
-        cam.setRotation(new Quaternion(0.027341122f, 0.9686672f, -0.12411701f, 0.21338053f));
+        cam.setLocation(new Vector3f(-700, 100, 300));
+        cam.setRotation(new Quaternion().fromAngles(new float[]{FastMath.PI*0.06f,FastMath.PI*0.65f,0}));
 
 
         Spatial sky = SkyFactory.createSky(assetManager, "Scenes/Beach/FullskiesSunset0068.dds", false);
@@ -93,12 +80,66 @@ public class TestPostWater extends SimpleApplication {
 
         fpp = new FilterPostProcessor(assetManager);
         water = new WaterFilter(rootNode, lightDir);
- 
+        water.setFoamHardness(0.6f);
+
         water.setWaterHeight(initialWaterHeight);
         fpp.addFilter(water);
         viewPort.addProcessor(fpp);
 
     }
+
+    private void createTerrain(Node rootNode) {
+        matRock = new Material(assetManager, "Common/MatDefs/Terrain/TerrainLighting.j3md");
+        matRock.setBoolean("useTriPlanarMapping", false);
+        matRock.setBoolean("WardIso", true);
+        matRock.setTexture("AlphaMap", assetManager.loadTexture("Textures/Terrain/splat/alphamap.png"));
+        Texture heightMapImage = assetManager.loadTexture("Textures/Terrain/splat/mountains512.png");
+        Texture grass = assetManager.loadTexture("Textures/Terrain/splat/grass.jpg");
+        grass.setWrap(WrapMode.Repeat);
+        matRock.setTexture("DiffuseMap", grass);
+        matRock.setFloat("DiffuseMap_0_scale", 64);
+        Texture dirt = assetManager.loadTexture("Textures/Terrain/splat/dirt.jpg");
+        dirt.setWrap(WrapMode.Repeat);
+        matRock.setTexture("DiffuseMap_1", dirt);
+        matRock.setFloat("DiffuseMap_1_scale", 16);
+        Texture rock = assetManager.loadTexture("Textures/Terrain/splat/road.jpg");
+        rock.setWrap(WrapMode.Repeat);
+        matRock.setTexture("DiffuseMap_2", rock);
+        matRock.setFloat("DiffuseMap_2_scale", 128);
+        Texture normalMap0 = assetManager.loadTexture("Textures/Terrain/splat/grass_normal.png");
+        normalMap0.setWrap(WrapMode.Repeat);
+        Texture normalMap1 = assetManager.loadTexture("Textures/Terrain/splat/dirt_normal.png");
+        normalMap1.setWrap(WrapMode.Repeat);
+        Texture normalMap2 = assetManager.loadTexture("Textures/Terrain/splat/road_normal.png");
+        normalMap2.setWrap(WrapMode.Repeat);
+        matRock.setTexture("NormalMap", normalMap0);
+        matRock.setTexture("NormalMap_1", normalMap2);
+        matRock.setTexture("NormalMap_2", normalMap2);
+
+        matWire = new Material(assetManager, "Common/MatDefs/Misc/WireColor.j3md");
+        matWire.setColor("Color", ColorRGBA.Green);
+        AbstractHeightMap heightmap = null;
+        try {
+            heightmap = new ImageBasedHeightMap(ImageToAwt.convert(heightMapImage.getImage(), false, true, 0), 0.25f);
+            heightmap.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        terrain = new TerrainQuad("terrain", 65, 513, heightmap.getHeightMap());
+        List<Camera> cameras = new ArrayList<Camera>();
+        cameras.add(getCamera());
+        terrain.setMaterial(matRock);
+        terrain.setLocalScale(new Vector3f(5,5,5));
+        terrain.setLocalTranslation(new Vector3f(0,-30,0));
+        terrain.setModelBound(new BoundingBox());
+        terrain.updateModelBound();
+        terrain.setLocked(false); // unlock it so we can edit the height
+
+        terrain.setShadowMode(ShadowMode.Receive);
+        rootNode.attachChild(terrain);
+
+    }
+
     //This part is to emulate tides, slightly varrying the height of the water plane
     private float time = 0.0f;
     private float waterHeight = 0.0f;
@@ -110,6 +151,5 @@ public class TestPostWater extends SimpleApplication {
         time += tpf;
         waterHeight = (float) Math.cos(((time * 0.6f) % FastMath.TWO_PI)) * 1.5f;
         water.setWaterHeight(initialWaterHeight + waterHeight);
-
     }
 }

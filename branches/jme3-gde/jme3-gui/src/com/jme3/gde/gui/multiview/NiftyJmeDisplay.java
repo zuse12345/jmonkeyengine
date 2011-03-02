cbed41ba3606c73ce0d862a0fba169a2b39ecfbf
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-03-02 15:28:01 +0000

    - fix nifty compile error

--- branches/jme3-gde/jme3-gui/src/com/jme3/gde/gui/multiview/NiftyJmeDisplay.java
+++ branches/jme3-gde/jme3-gui/src/com/jme3/gde/gui/multiview/NiftyJmeDisplay.java
@@ -34,6 +34,7 @@ package com.jme3.gde.gui.multiview;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.audio.AudioRenderer;
+import com.jme3.gde.core.scene.SceneApplication;
 import com.jme3.niftygui.InputSystemJme;
 import com.jme3.niftygui.RenderDeviceJme;
 import com.jme3.niftygui.SoundDeviceJme;
@@ -54,7 +55,7 @@ public class NiftyJmeDisplay extends com.jme3.niftygui.NiftyJmeDisplay implement
                            ViewPort vp){
         this.assetManager = assetManager;
         //FIXME: needed, else npe
-        this.inputSys = new InputSystemJme();
+        this.inputSys = new InputSystemJme(SceneApplication.getApplication().getInputManager());
 
         w = vp.getCamera().getWidth();
         h = vp.getCamera().getHeight();

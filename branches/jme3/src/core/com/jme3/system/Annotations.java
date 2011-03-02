b2b988b88222903daf2d0573e47e196868c1c7ba
normen667 <normen667@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-03-02 10:30:50 +0000

    - add checker qualifier classes

--- branches/jme3/src/core/com/jme3/system/Annotations.java
+++ branches/jme3/src/core/com/jme3/system/Annotations.java
@@ -31,6 +31,13 @@
  */
 package com.jme3.system;
 
+import checkers.quals.TypeQualifier;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * This class contains the Annotation definitions for jME3. Mostly these are used
  * for code error checking.
@@ -42,21 +49,28 @@ public class Annotations {
      * Annotation used for math primitive fields, method parameters or method return values.
      * Specifies that the primitve is read only and should not be changed.
      */
-    public @interface ReadOnly{
+    @Documented
+    @Retention(RetentionPolicy.RUNTIME)
+    @TypeQualifier
+    @Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.TYPE, ElementType.METHOD})
+    public @interface ReadOnly {
     }
 
     /**
      * Annotation used for methods in math primitives that are destructive to the
      * object (xxxLocal, setXXX etc.).
      */
-    public @interface Destructive{
+    @Documented
+    @Retention(RetentionPolicy.RUNTIME)
+    @TypeQualifier
+    @Target({ElementType.METHOD})
+    public @interface Destructive {
     }
 
     /**
      * Annotation used for public methods that are not to be called by users.
      * Examples include update() methods etc.
      */
-    public @interface Internal{
+    public @interface Internal {
     }
-
 }

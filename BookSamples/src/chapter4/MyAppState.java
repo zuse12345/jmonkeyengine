5b46bb60f58f7fa0977dffafe226c57225b5f4f6
rootli <rootli@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-06-03 18:54:58 +0000

    sample code for chapters (booksamples)

---
+++ BookSamples/src/chapter4/MyAppState.java
@@ -0,0 +1,47 @@
+package solutions.chapter4;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+
+/** A template how to create an Application State.
+ * This example state simply changes the background color 
+ * depending on the camera position. */
+public class MyAppState extends AbstractAppState {
+
+  private Application app;
+
+  @Override
+  public void update(float tpf) {
+    Vector3f v = app.getViewPort().getCamera().getLocation();
+    app.getViewPort().setBackgroundColor(new ColorRGBA(v.getX()/10,v.getY()/10,v.getZ()/10,1));
+  }
+
+  @Override
+  public void initialize(AppStateManager stateManager, Application app) {
+    super.initialize(stateManager, app);
+    this.app = app;
+  }
+
+  @Override
+  public void setEnabled(boolean enabled) {
+    super.setEnabled(enabled);
+  }
+
+  @Override
+  public void stateAttached(AppStateManager stateManager) {
+    super.stateAttached(stateManager);
+  }
+
+  @Override
+  public void stateDetached(AppStateManager stateManager) {
+    super.stateDetached(stateManager);
+  }
+
+  public void setApp(Application app) {
+    this.app = app;
+  }
+  
+}
---
+++ BookSamples/src/chapter5/LoadScene.java
@@ -0,0 +1,54 @@
+package solutions.chapter5;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.HttpZipLocator;
+import com.jme3.asset.plugins.ZipLocator;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.MaterialList;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.plugins.ogre.OgreMeshKey;
+
+/**  */
+public class LoadScene extends SimpleApplication {
+  
+  private AnimChannel channel;
+  private AnimControl control;
+  private Node player;
+  
+  public static void main(String[] args) {
+    LoadScene app = new LoadScene();
+    app.start();
+  }
+  
+  @Override
+  public void simpleInitApp() {
+    viewPort.setBackgroundColor(ColorRGBA.LightGray);
+    
+    DirectionalLight sun = new DirectionalLight();
+    sun.setDirection(new Vector3f(1, 0, -2).normalizeLocal());
+    sun.setColor(ColorRGBA.White);
+    rootNode.addLight(sun);
+    
+    assetManager.registerLocator(
+    "http://jmonkeyengine.googlecode.com/files/quake3level.zip", 
+    HttpZipLocator.class.getName());
+    MaterialList matList = (MaterialList) assetManager.loadAsset("Scene.material");
+    OgreMeshKey key = new OgreMeshKey("main.meshxml", matList);
+    Node gameLevel = (Node) assetManager.loadAsset(key);
+    gameLevel.setLocalScale(0.1f);
+    rootNode.attachChild(gameLevel);
+    
+//    assetManager.registerLocator("assets/Models/town.zip",
+//            ZipLocator.class.getName());
+//    assetManager.registerLocator("town.zip", ZipLocator.class.getName());
+//    Spatial sceneModel = assetManager.loadModel("main.scene");
+//    sceneModel.setLocalScale(2f);
+//    rootNode.attachChild(sceneModel);
+    
+  }
+}

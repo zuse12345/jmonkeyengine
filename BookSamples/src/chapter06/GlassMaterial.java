b5d5c0901548f38ee81fdd132fbc42aaf46afd0c
rootli <rootli@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2011-10-02 20:17:28 +0000

    refactored sample

--- BookSamples/src/chapter06/GlassMaterial.java
+++ BookSamples/src/chapter06/GlassMaterial.java
@@ -5,10 +5,12 @@ import com.jme3.light.*;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Cylinder;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.util.TangentBinormalGenerator;
 
@@ -26,53 +28,43 @@ public class GlassMaterial extends SimpleApplication {
    
     /** A wall with a rough bricky surface */
     Box box = new Box(2,2,2);
-    Geometry wall = new Geometry("Shiny rock", box);
-    Material mat_wall = new Material(assetManager, 
+    Geometry wall = new Geometry("Wall", box);
+    
+    Material wall_mat = new Material(assetManager, 
             "Common/MatDefs/Light/Lighting.j3md");
-    mat_wall.setTexture("DiffuseMap",
+    wall_mat.setTexture("DiffuseMap",
             assetManager.loadTexture("Textures/BrickWall/BrickWall_diffuse.jpg"));
-    mat_wall.setTexture("NormalMap",
+    wall_mat.setTexture("NormalMap",
             assetManager.loadTexture("Textures/BrickWall/BrickWall_normal.jpg"));
-    mat_wall.setFloat("Shininess", 0f); // [0,128]
-    wall.setMaterial(mat_wall);
+    wall.setMaterial(wall_mat);
     wall.setLocalTranslation(0,-3,0);   // Move it a bit
     rootNode.attachChild(wall);
  
     Sphere sphere = new Sphere(32,32, 2f);
-    Geometry glasball = new Geometry("normal sphere", sphere);
-    Material mat_glass = new Material(assetManager, 
+    Geometry glass_geo = new Geometry("normal sphere", sphere);
+    Material glass_mat = new Material(assetManager, 
             "Common/MatDefs/Light/Lighting.j3md");
-    mat_glass.setBoolean("UseMaterialColors",true);
-    mat_glass.setBoolean("UseAlpha",true);
-    mat_glass.setFloat("AlphaDiscardThreshold",0.0f);
-    mat_glass.setTexture("AlphaMap", assetManager.loadTexture("Textures/bark.png"));
-    mat_glass.setColor("Ambient", ColorRGBA.Cyan );
-    mat_glass.setColor("Diffuse", ColorRGBA.Cyan );
-    mat_glass.setColor("Specular", ColorRGBA.White );
-    mat_glass.setFloat("Shininess", 16f);    // [0,128]
-    mat_glass.getAdditionalRenderState().setBlendMode(BlendMode.Color);
-    mat_glass.getAdditionalRenderState().setDepthWrite(false);
-    glasball.setQueueBucket(Bucket.Transparent);
-    glasball.setMaterial(mat_glass);
-    glasball.move(0f, 1, 0);
-    rootNode.attachChild(glasball); 
+    glass_mat.setBoolean("UseMaterialColors",true);
+    glass_mat.setBoolean("UseAlpha",true);
+    glass_mat.setFloat("AlphaDiscardThreshold",0.1f);
+    glass_mat.setTexture("AlphaMap", assetManager.loadTexture("Textures/rock.png"));
+    glass_mat.setColor("Ambient", ColorRGBA.Cyan );
+    glass_mat.setColor("Diffuse", ColorRGBA.Cyan );
+    glass_mat.setColor("Specular", ColorRGBA.White );
+    glass_mat.setFloat("Shininess", 16f);    // [0,128]
+    glass_mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+    glass_mat.getAdditionalRenderState().setDepthWrite(false);
+    glass_geo.setQueueBucket(Bucket.Translucent);
+    glass_geo.setMaterial(glass_mat);
+    glass_geo.move(0f, 1f, 0);
+    glass_geo.rotate(FastMath.DEG_TO_RAD*90, 0, 0);
+    rootNode.attachChild(glass_geo); 
 
-    
     /** Must add a light to make the lit object visible! */
     DirectionalLight sun = new DirectionalLight();
-    sun.setDirection(new Vector3f(1,-2f,-1.5f).normalizeLocal());
+    sun.setDirection(new Vector3f(.3f,-0.5f,-0.5f));
     sun.setColor(ColorRGBA.White);
     rootNode.addLight(sun);
-    
-    /** A white, spot light source. */ 
-//    PointLight lamp = new PointLight();
-//    lamp.setPosition(new Vector3f(-3,3,5));
-//    lamp.setColor(ColorRGBA.White);
-//    rootNode.addLight(lamp); 
-    
-        /** A white ambient light source. */ 
-//    AmbientLight ambient = new AmbientLight();
-//    ambient.setColor(ColorRGBA.White.mult(5f));
-//    rootNode.addLight(ambient); 
+
   }
 }
